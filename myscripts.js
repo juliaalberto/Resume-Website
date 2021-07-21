a8a399527e0714cae51cd99e789c313778e5d948
Julia Alberto <julia.elyssa.alberto@gmail.com>
2021-07-20 20:31:29 -0700

    added jumping button

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,10 @@
-
 function changemenu(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
 
-    var y = document.getElementById("myLinks");
-    if (y.style.display === "block") {
-      y.style.display = "none";
-    } else {
-      y.style.display = "block";
-    }
+  var y = document.getElementById("myLinks");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
 }
